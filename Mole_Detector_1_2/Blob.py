df50c8a0e16942b096f0d9918c5d58931e7b230e
Stephane Hatgis-Kessell <2020shatgiskessell@05196.dwight.edu>
2019-10-03 14:36:54 -0400

    removed none-branch files

--- Mole_Detector_1_2/Blob.py
+++
@@ -1,9 +0,0 @@
-class Blob ():
-    def __init__ (self, x = None,y = None,area = None, roi = None, matrix = None, radius = None, color = None):
-        self.x = x
-        self.y = y
-        self.area = area
-        self.roi = roi
-        self.matrix = matrix
-        self.radius = radius
-        self.color = color
--- mole_tracker_app/Pods/Target Support Files/Pods-mole_tracker_app/Pods-mole_tracker_app-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mole_tracker_app : NSObject
-@end
-@implementation PodsDummy_Pods_mole_tracker_app
-@end
